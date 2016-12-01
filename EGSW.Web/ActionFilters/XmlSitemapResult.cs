3e6e4846a19f4a0b761cf8c1c8d86d4dba38ff19
arpan209 <arpan209@gmail.com>
2016-12-01 15:48:13 -0500

    Revert "Goolge analytics code"
    This reverts commit 7a29fd598fb24c0760672a6d2d70f7c385e90aff.

--- EGSW.Web/ActionFilters/XmlSitemapResult.cs
+++ EGSW.Web/ActionFilters/XmlSitemapResult.cs
@@ -20,19 +20,12 @@ namespace EGSW.Web.ActionFilters
         public override void ExecuteResult(ControllerContext context)
         {
             string encoding = context.HttpContext.Response.ContentEncoding.WebName;
-            //XDocument sitemap = new XDocument(new XDeclaration("1.0", "utf-8", "yes"),
-            //     new XElement("urlset", new XAttribute(XNamespace.Xmlns.GetName("Xmlns"), "http://www.google.com/schemas/sitemap-news/0.9"),
-            //          from item in _items
-            //          select CreateItemElement(item)
-            //          )
-            //     );
-
             XDocument sitemap = new XDocument(new XDeclaration("1.0", "utf-8", "yes"),
-                new XElement("urlset", new XAttribute("Xmlns", "http://www.sitemaps.org/schemas/sitemap/0.9"),
-                     from item in _items
-                     select CreateItemElement(item)
-                     )
-                );
+                 new XElement("urlset", new XAttribute(XNamespace.Xmlns.GetName("Xmlns"), "http://www.sitemaps.org/schemas/sitemap/0.84"),
+                      from item in _items
+                      select CreateItemElement(item)
+                      )
+                 );
 
             context.HttpContext.Response.ContentType = "text/xml";
             context.HttpContext.Response.Flush();
@@ -41,10 +34,7 @@ namespace EGSW.Web.ActionFilters
 
         private XElement CreateItemElement(ISitemapItem item)
         {
-            string url = item.Url.Substring(item.Url.Length - 1, 1).Contains("/") ? item.Url.ToLower() : item.Url.ToLower() + "/"; 
-
-            XElement itemElement = new XElement("url", new XElement("loc", url));
-            
+            XElement itemElement = new XElement("url", new XElement("loc", item.Url.ToLower()));
 
             if (item.LastModified.HasValue)
                 itemElement.Add(new XElement("lastmod", item.LastModified.Value.ToString("yyyy-MM-dd")));
